9bafb2e5b0509092a70a60c0894ae6e1d7e4917d
libuse.poustkova <libuse.poustkova@skoda.cz>
2021-04-13 19:45:35 +0200

    pridane tlacitko losuj

--- sportka.js
+++ sportka.js
@@ -10,20 +10,22 @@
 let osudi = [];
 let vyherniCisla = document.querySelector('#vyherni-cisla');
 let tazenaCisla = [];
-for (let i =1; i <= 49; i = i + 1) {
-    osudi.push(i);
-}
-// console.log(osudi)
+ for (let i =1; i <= 49; i = i + 1) {
+        osudi.push(i);
+    }
 
-for (let i = 0; i < 7; i++){
-    let vyherniIndex = Math.floor(Math.random() * osudi.length);
-    let vyherniCislo = osudi[vyherniIndex];
+function losuj() {
+    vyherniCisla.innerHTML = '';
+    tazenaCisla = [];
 
-    tazenaCisla.push(vyherniCislo);
-    osudi.splice(vyherniIndex, 1);
-}
+    for (let i = 0; i < 7; i++){
+        let vyherniIndex = Math.floor(Math.random() * osudi.length);
+        let vyherniCislo = osudi[vyherniIndex];
 
-
-for (let i = 0; i < tazenaCisla.length; i++) {
-    vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span> ';
+        tazenaCisla.push(vyherniCislo);
+        osudi.splice(vyherniIndex, 1);
+    }
+    for (let i = 0; i < tazenaCisla.length; i++) {
+        vyherniCisla.innerHTML += '<span class="cislo">' + tazenaCisla[i] + '</span> ';
+  }
 }
